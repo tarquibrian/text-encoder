78d8e5a6e51ee2fc9b38cdb0bfa97e8faafea2c1
brian tarqui <tarquibrian@gmail.com>
2023-05-29 19:21:28 -0400

    add

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 const encriptar = () => {
   const list = document.getElementById("listContent");
-  const newRow = document.createElement("div");
-  const msg = "hola gente";
-  newRow.innerHTML = `
+  const InputText = document.getElementById("input-text");
+  const checkbox = document.getElementById("checkbox");
+
+  if (InputText.value !== "") {
+    const newRow = document.createElement("div");
+
+    const textEncoded =
+      checkbox.value === "on"
+        ? encriptarTexto(InputText.value)
+        : desencriptarTexto(InputText.value);
+
+    newRow.innerHTML = `
   <div class="row">
     <span class="row-item"
-      >items1 </span
+      >${textEncoded}</span
     >
     <div class="list-options">
       <button>
@@ -18,6 +27,58 @@ const encriptar = () => {
     </div>
   </div>
   `;
-  list.appendChild(newRow);
-  console.log(list);
+
+    InputText.value = "";
+    list.appendChild(newRow);
+  }
+};
+
+const encriptarTexto = (stringEncriptada) => {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
+    }
+  }
+  return stringEncriptada;
+};
+
+const desencriptarTexto = (stringDesencriptada) => {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringDesencriptada = stringDesencriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+      stringDesencriptada = stringDesencriptada.replaceAll(
+        matrizCodigo[i][1],
+        matrizCodigo[i][0]
+      );
+    }
+  }
+  return stringDesencriptada;
+};
+
+const changeValue = () => {
+  const btnEncode = document.getElementById("btnEncode");
+  const checkbox = document.getElementById("checkbox");
+  checkbox.value = checkbox.value === "on" ? "off" : "on";
+  btnEncode.textContent =
+    checkbox.value === "on" ? "Encriptar" : "Desencriptar";
 };
